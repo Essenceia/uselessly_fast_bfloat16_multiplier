43742323c9eeaca2a57ee696bfcec7e145e7c08a
Julia Desmazes <julia.desmazes@gmail.com>
2026-03-28 09:13:11 -0500

    simple xprop sim test

--- test/test.py
+++ test/test.py
@@ -20,20 +20,37 @@ async def rst(dut, ena=1):
 	dut.ena.value = 1
 	await ClockCycles(dut.clk, 10)
 
-async def test_mul(dut):
+async def test_xprop(dut):
 	dut.uio_in.value = 0 
 	dut.ui_in.value = LogicArray("XXXXXXXX",8)
 	await ClockCycles(dut.clk, 2)
+	# send data
 	dut.uio_in.value = 1
 	dut.ui_in.value = 0
 	await ClockCycles(dut.clk, 4)
-	dut.uio_in.value = 0
+	
+	# wait for prop 
 	dut.ui_in.value = LogicArray("XXXXXXXX",8)
-	await ClockCycles(dut.clk, 10)
+	dut.uio_in.value = 0
+	assert(dut.uio_out.value[7] == 0)
+	assert(dut.uio_out.value[6] == 0)
+	await ClockCycles(dut.clk, 2)
+	
+	# check early 
+	assert(dut.uio_out.value[7] == 1)
+	assert(dut.uio_out.value[6] == 0)
+	await ClockCycles(dut.clk, 1)
+	
+	# check res
+	for i in range(0,1):
+		assert(dut.uio_out.value[7] == 1)
+		assert(dut.uio_out.value[6] == 1)
+		assert(dut.uo_out.value == 0)
+		await ClockCycles(dut.clk, 1)
  
 @cocotb.test()
 async def test_project(dut):
-	dut._log.info("Trust me bro")
 	await rst(dut)
-	await test_mul(dut)
+	await test_xprop(dut)
+	await ClockCycles(dut.clk, 10)
 
